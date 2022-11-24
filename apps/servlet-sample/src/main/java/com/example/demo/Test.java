16e29178c56f03c1ba32d8462542c72bf16ec138
jpra1113 <jpra1113@gmail.com>
2022-11-24 14:26:47 +0800

    WIP

--- apps/servlet-sample/src/main/java/com/example/demo/Test.java
+++ apps/servlet-sample/src/main/java/com/example/demo/Test.java
@@ -1,20 +1,17 @@
 package com.example.demo;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HelloWorld extends HttpServlet {
+public class Test extends HttpServlet {
 
   public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws IOException, ServletException {
-    response.setContentType("text/html");
-
-    PrintWriter out = response.getWriter();
-    out.print("Hello World!! HttpServlet");
+    request.setAttribute("text", "Hello World");
+    request.getRequestDispatcher("test.jsp").forward(request, response);
   }
 }
---
+++ apps/servlet-sample/src/main/webapp/test.jsp
@@ -0,0 +1,11 @@
+<%@ page language="java" contentType="text/html; charset=BIG5" pageEncoding="BIG5"%>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="BIG5" />
+    <title>Insert title here</title>
+  </head>
+  <body>
+    text: <%= request.getAttribute("text") %>
+  </body>
+</html>
